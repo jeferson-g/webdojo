60fbcfbfc554d933538ac7a6ad3700a182dfa263
JRomeo11 <jefersonntx@gmail.com>
2025-08-14 12:42:13 -0300

    Automatização preenchendo campos

--- cypress/e2e/Consultancy.cy.js
+++ cypress/e2e/Consultancy.cy.js
@@ -6,6 +6,20 @@ describe('Formulário de consultoria', () => {
         cy.SubmitLoginform('papito@webdojo.com', 'katana123')
 
         cy.goTo('Formulários', 'Consultoria')
+//seguindo a automação sem usar o ID para um melhor aprendizado
+        cy.get('input[placeholder="Digite seu nome completo"]').type('Maria Antonieta')
+        cy.get('input[placeholder="Digite seu email"]').type('Antonieta@teste.com')
+
+//Aqui houve uma validação para saber se o campo estava sendo formatado corretamente após o cliente digitar os números
+        cy.get('input[placeholder="(00) 00000-0000"]').type('11800028922')
+          .should('have.value', '(11) 80002-8922')
+        
+//Forma alternativa de interagir caso não tenha ID ou placeholder
+        cy.contains('label', 'Tipo de Consultoria')
+          .parent()
+          .find('select')
+          .select('In Company')
+
     })
 
 })
