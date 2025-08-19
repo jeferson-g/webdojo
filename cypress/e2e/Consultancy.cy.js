f521510368f50206158d7957d3b24e4b1a210bbd
JRomeo11 <jefersonntx@gmail.com>
2025-08-18 21:40:41 -0300

    Automação de botão+c.texto

--- cypress/e2e/Consultancy.cy.js
+++ cypress/e2e/Consultancy.cy.js
@@ -10,11 +10,11 @@ describe('Formulário de consultoria', () => {
         cy.get('input[placeholder="Digite seu nome completo"]').type('Maria Antonieta')
         cy.get('input[placeholder="Digite seu email"]').type('Antonieta@teste.com')
 
-        //Aqui houve uma validação para saber se o campo estava sendo formatado corretamente após o cliente digitar os números
+        //Aqui houve uma validação para saber se o campo estava sendo formatado corretamente após o cliente digitar os números.
         cy.get('input[placeholder="(00) 00000-0000"]').type('11800028922')
             .should('have.value', '(11) 80002-8922')
 
-        //Forma alternativa de interagir caso não tenha ID ou placeholder
+        //Forma alternativa de interagir caso não tenha ID ou placeholder.
         cy.contains('label', 'Tipo de Consultoria')
             .parent()
             .find('select')
@@ -33,6 +33,35 @@ describe('Formulário de consultoria', () => {
 
         cy.get('input[placeholder="000.000.000-00"]').type('60780967246')
             .should('have.value', '607.809.672-46')
+
+        //Aqui por melhor prática, é essencial criar um loop para evitar as repetições.
+
+        const discoveryChannels = [
+            'Instagram',
+            'LinkedIn',
+            'Udemy',
+            'YouTube',
+            'Indicação de Amigo'
+        ]
+
+        discoveryChannels.forEach((channel)  => {
+            cy.contains('label', channel)
+                .find('input')
+                .check()
+                .should('be.checked')
+        })
+
+        //Devido ao botão estar com o elemento "hidden", o cypress não aceita a interação necessitando de usar o "force:true"
+        cy.get('input[type="file"]')
+            .selectFile('./cypress/fixtures/testfile.pdf', {force:true})
+
+
+        cy.get('textarea[placeholder="Descreva mais detalhes sobre sua necessidade"]')
+            .type('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged')
+
+
+
     })
+    
 
 })
