8291c0dd4ad9bee7c2ae3e182093d3fbca405a9e
JRomeo11 <jefersonntx@gmail.com>
2025-08-18 22:50:35 -0300

    finalizando ás ultimas validações do formulario

--- cypress/e2e/Consultancy.cy.js
+++ cypress/e2e/Consultancy.cy.js
@@ -34,7 +34,7 @@ describe('Formulário de consultoria', () => {
         cy.get('input[placeholder="000.000.000-00"]').type('60780967246')
             .should('have.value', '607.809.672-46')
 
-        //Aqui por melhor prática, é essencial criar um loop para evitar as repetições.
+        //Aqui por melhor prática, é essencial criar um arraw e um loop para evitar as repetições de código.
 
         const discoveryChannels = [
             'Instagram',
@@ -44,7 +44,7 @@ describe('Formulário de consultoria', () => {
             'Indicação de Amigo'
         ]
 
-        discoveryChannels.forEach((channel)  => {
+        discoveryChannels.forEach((channel) => {
             cy.contains('label', channel)
                 .find('input')
                 .check()
@@ -53,15 +53,42 @@ describe('Formulário de consultoria', () => {
 
         //Devido ao botão estar com o elemento "hidden", o cypress não aceita a interação necessitando de usar o "force:true"
         cy.get('input[type="file"]')
-            .selectFile('./cypress/fixtures/testfile.pdf', {force:true})
+            .selectFile('./cypress/fixtures/testfile.pdf', { force: true })
 
 
         cy.get('textarea[placeholder="Descreva mais detalhes sobre sua necessidade"]')
-            .type('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged')
+            .type('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s')
 
+            
+        const techs = [
+            'Cypress',
+            'Selenium',
+            'Robot Framework'
+        ]
 
+        techs.forEach((tech) => {
+            cy.get('input[placeholder="Digite uma tecnologia e pressione Enter"]')
+                .type(tech)
+                .type('{enter}')
 
+            cy.contains('label', 'Tecnologias')
+                .parent()
+                .contains('span', tech)
+                .should('be.visible')
+
+        })
+
+        cy.contains('label', 'termos de uso')
+            .find('input')
+            .check()
+
+        cy.contains('button', 'Enviar formulário')
+            .click()
+
+        cy.contains('Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido.')
+            .should('be.visible')
+        
     })
-    
+
 
 })
