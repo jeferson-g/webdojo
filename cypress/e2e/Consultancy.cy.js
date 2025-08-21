b6175cfc81cbe2c7b71e53643f7d7aa76a6c577f
JRomeo11 <jefersonntx@gmail.com>
2025-08-21 12:35:42 -0300

    Validação dos campos obrigatorios

--- cypress/e2e/Consultancy.cy.js
+++ cypress/e2e/Consultancy.cy.js
@@ -88,7 +88,32 @@ describe('Formulário de consultoria', () => {
         cy.contains('Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido.')
             .should('be.visible')
         
-    })
+        })
 
+    it.only('Deve verificar os campos obrigatórios', () => {
+        cy.Start()
+        cy.SubmitLoginform('papito@webdojo.com', 'katana123')
+
+        cy.goTo('Formulários', 'Consultoria')
+
+        cy.contains('button', 'Enviar formulário')
+            .click()
+            
+        cy.contains('p', "Digite nome e sobrenome")
+            .should('be.visible')
+            .and('have.class','text-red-400')
+            .and('have.css','color', 'rgb(248, 113, 113)')
+
+        cy.contains('p', "Informe um email válido")
+            .should('be.visible')
+            .and('have.class','text-red-400')
+            .and('have.css','color', 'rgb(248, 113, 113)')
+
+        cy.contains('p', "Você precisa aceitar os termos de uso")
+            .should('be.visible')
+            .and('have.class','text-red-400')
+            .and('have.css','color', 'rgb(248, 113, 113)')
+    
+    })
 
 })
