509aceea613e0d5f3d0e5cd3934b25378ee58743
JRomeo11 <jefersonntx@gmail.com>
2025-09-08 20:02:03 -0300

    Gerenciamento de massa

--- cypress/e2e/Consultancy.cy.js
+++ cypress/e2e/Consultancy.cy.js
@@ -1,51 +1,70 @@
 describe('Formulário de consultoria', () => {
 
-    beforeEach(()=>{
+    beforeEach(() => {
         cy.login()
-        cy.goTo('Formulários', 'Consultoria') 
+        cy.goTo('Formulários', 'Consultoria')
     })
 
     it.only('Deve solicitar consultoria individual', () => {
 
+        const consultancyform = {
+            name: 'Maria Antonieta',
+            email: 'Antonieta@teste.com',
+            phone: '(11) 80002-8922',
+            consultancytype: 'Individual',
+            persontype: 'Pessoa Física',
+            document: '607.809.672-46',
+            discoveryChannels: [
+                'Instagram',
+                'LinkedIn',
+                'Udemy',
+                'YouTube',
+                'Indicação de Amigo'
+            ],
+            file: './cypress/fixtures/testfile.pdf',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+            techs: [
+                'Cypress',
+                'Selenium',
+                'Robot Framework'
+            ],
+            terms: true
+        }
+
         //seguindo a automação sem usar o ID para um melhor aprendizado
-        cy.get('input[placeholder="Digite seu nome completo"]').type('Maria Antonieta')
-        cy.get('input[placeholder="Digite seu email"]').type('Antonieta@teste.com')
+        cy.get('input[placeholder="Digite seu nome completo"]').type(consultancyform.name)
+        cy.get('input[placeholder="Digite seu email"]').type(consultancyform.email)
 
         //Aqui houve uma validação para saber se o campo estava sendo formatado corretamente após o cliente digitar os números.
-        cy.get('input[placeholder="(00) 00000-0000"]').type('11800028922')
+        cy.get('input[placeholder="(00) 00000-0000"]').type(consultancyform.phone)
             .should('have.value', '(11) 80002-8922')
 
         //Forma alternativa de interagir caso não tenha ID ou placeholder.
         cy.contains('label', 'Tipo de Consultoria')
             .parent()
             .find('select')
-            .select('In Company')
+            .select(consultancyform.consultancytype)
 
 
-        cy.contains('label', 'Pessoa Física')
-            .find('input')
-            .check()
-            .should('be.checked')
+        if (consultancyform.persontype === 'Pessoa Física') {
+            cy.contains('label', 'Pessoa Física')
+                .find('input')
+                .check()
+                .should('be.checked')
 
-        cy.contains('label', 'Pessoa Jurídica')
-            .find('input')
-            .should('be.not.checked')
+            cy.contains('label', 'Pessoa Jurídica')
+                .find('input')
+                .should('be.not.checked')
+        }
 
 
-        cy.get('input[placeholder="000.000.000-00"]').type('60780967246')
+        cy.get('input[placeholder="000.000.000-00"]').type(consultancyform.document)
             .should('have.value', '607.809.672-46')
 
         //Aqui por melhor prática, é essencial criar um arraw e um loop para evitar as repetições de código.
 
-        const discoveryChannels = [
-            'Instagram',
-            'LinkedIn',
-            'Udemy',
-            'YouTube',
-            'Indicação de Amigo'
-        ]
 
-        discoveryChannels.forEach((channel) => {
+        consultancyform.discoveryChannels.forEach((channel) => {
             cy.contains('label', channel)
                 .find('input')
                 .check()
@@ -54,20 +73,14 @@ describe('Formulário de consultoria', () => {
 
         //Devido ao botão estar com o elemento "hidden", o cypress não aceita a interação necessitando de usar o "force:true"
         cy.get('input[type="file"]')
-            .selectFile('./cypress/fixtures/testfile.pdf', { force: true })
+            .selectFile(consultancyform.file, { force: true })
 
 
         cy.get('textarea[placeholder="Descreva mais detalhes sobre sua necessidade"]')
-            .type('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s')
+            .type(consultancyform.description)
 
 
-        const techs = [
-            'Cypress',
-            'Selenium',
-            'Robot Framework'
-        ]
-
-        techs.forEach((tech) => {
+        consultancyform.techs.forEach((tech) => {
             cy.get('input[placeholder="Digite uma tecnologia e pressione Enter"]')
                 .type(tech)
                 .type('{enter}')
@@ -79,9 +92,11 @@ describe('Formulário de consultoria', () => {
 
         })
 
-        cy.contains('label', 'termos de uso')
-            .find('input')
-            .check()
+        if (consultancyform.terms === true) {
+            cy.contains('label', 'termos de uso')
+                .find('input')
+                .check()
+        }
 
         cy.contains('button', 'Enviar formulário')
             .click()
@@ -95,6 +110,109 @@ describe('Formulário de consultoria', () => {
         // cy.contains('Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido.')
         //     .should('be.visible')
 
+    })
+
+    it('Deve solicitar consultoria In Company', () => {
+
+        const consultancyform = {
+            name: 'Maria Antonieta',
+            email: 'Antonieta@teste.com',
+            phone: '(11) 80002-8922',
+            consultancytype: 'In Company',
+            persontype: 'Pessoa Jurídica',
+            document: '63.715.220/0001-11',
+            discoveryChannels: [
+                'Instagram',
+                'LinkedIn',
+                'Udemy',
+                'YouTube',
+                'Indicação de Amigo'
+            ],
+            file: './cypress/fixtures/testfile.pdf',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+            techs: [
+                'Cypress',
+                'Selenium',
+                'Robot Framework'
+            ],
+            terms: true
+        }
+
+        //seguindo a automação sem usar o ID para um melhor aprendizado
+        cy.get('input[placeholder="Digite seu nome completo"]').type(consultancyform.name)
+        cy.get('input[placeholder="Digite seu email"]').type(consultancyform.email)
+
+        //Aqui houve uma validação para saber se o campo estava sendo formatado corretamente após o cliente digitar os números.
+        cy.get('input[placeholder="(00) 00000-0000"]').type(consultancyform.phone)
+            .should('have.value', '(11) 80002-8922')
+
+        //Forma alternativa de interagir caso não tenha ID ou placeholder.
+        cy.contains('label', 'Tipo de Consultoria')
+            .parent()
+            .find('select')
+            .select(consultancyform.consultancytype)
+
+
+        if (consultancyform.persontype === 'Pessoa Jurídica') {
+            cy.contains('label', 'Pessoa Jurídica')
+                .find('input')
+                .check()
+                .should('be.checked')
+
+            cy.contains('label', 'Pessoa Física')
+                .find('input')
+                .should('be.not.checked')
+        }
+
+
+        cy.get('input[placeholder="00.000.000/0000-00"]').type(consultancyform.document)
+        //.should('have.value', '607.809.672-46')
+
+        //Aqui por melhor prática, é essencial criar um arraw e um loop para evitar as repetições de código.
+
+
+        consultancyform.discoveryChannels.forEach((channel) => {
+            cy.contains('label', channel)
+                .find('input')
+                .check()
+                .should('be.checked')
+        })
+
+        //Devido ao botão estar com o elemento "hidden", o cypress não aceita a interação necessitando de usar o "force:true"
+        cy.get('input[type="file"]')
+            .selectFile(consultancyform.file, { force: true })
+
+
+        cy.get('textarea[placeholder="Descreva mais detalhes sobre sua necessidade"]')
+            .type(consultancyform.description)
+
+
+        consultancyform.techs.forEach((tech) => {
+            cy.get('input[placeholder="Digite uma tecnologia e pressione Enter"]')
+                .type(tech)
+                .type('{enter}')
+
+            cy.contains('label', 'Tecnologias')
+                .parent()
+                .contains('span', tech)
+                .should('be.visible')
+
+        })
+
+        if (consultancyform.terms === true) {
+            cy.contains('label', 'termos de uso')
+                .find('input')
+                .check()
+        }
+
+        cy.contains('button', 'Enviar formulário')
+            .click()
+
+        cy.get('.modal', { timeout: 7000 })
+            .should('be.visible')
+            .find('.modal-content')
+            .should('be.visible')
+            .and('have.text', 'Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido.')
     })
 
     it('Deve verificar os campos obrigatórios', () => {
@@ -132,12 +250,12 @@ describe('Formulário de consultoria', () => {
             .and('have.css', 'color', 'rgb(248, 113, 113)')
     })
 
-    afterEach(()=> {
+    afterEach(() => {
         cy.log('Teste aftereach, acontece a CADA TESTE')
     })
 
     after(() => {
-       cy.log('Teste after, Isso acontece depois de TODOS OS TESTES uma única vez') 
+        cy.log('Teste after, Isso acontece depois de TODOS OS TESTES uma única vez')
     })
 
 })
